e1d25cc31ef7f4f5f8db731119f7bc92358ecf42
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-06 18:07:22 +0530

    Create Buttons.py

---
+++ SessionBot/Plugins/Buttons.py
@@ -0,0 +1,11 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+buttons = [
+        [InlineKeyboardButton("𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/xemishra")],
+        [
+            InlineKeyboardButton("𝖧𝖾𝗅𝗉", callback_data="help"),
+            InlineKeyboardButton("𝖠𝖻𝗈𝗎𝗍", callback_data="about")
+        ],
+        [InlineKeyboardButton("𝖴𝗉𝖽𝖺𝗍𝖾𝗌", url="https://t.me/xemishra")],
+]
