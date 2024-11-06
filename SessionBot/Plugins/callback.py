6cda8ce5038bd91cdd6288910cb0c38f258edc1c
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-06 21:17:30 +0530

    Update callback.py

--- SessionBot/Plugins/callback.py
+++ SessionBot/Plugins/callback.py
@@ -1,1 +1,26 @@
+from pyrogram import filters, Client
+from pyrogram.types import CallbackQuery
+from SessionBot.Plugins.inline import gen_key
+from SessionBot.Plugins.generate import gen_session
 
+@Client.on_callback_query(
+    filters.regex(pattern=r"^(gensession|pyrogram|pyrogram1|telethon)$")
+)
+async def cb_choose(_, cq: CallbackQuery):
+    await cq.answer()
+    query = cq.matches[0].group(1)
+    if query == "gensession":
+        return await cq.message.reply_text(
+            text="<b>» 𝖢𝗅𝗂𝖼𝗄 𝖮𝗇 𝖳𝗁𝖾 𝖡𝗎𝗍𝗍𝗈𝗇𝗌 𝖡𝖾𝗅𝗈𝗐 𝖥𝗈𝗋 𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝗂𝗇𝗀 𝖸𝗈𝗎𝗋 𝖲𝖾𝗌𝗌𝗂𝗈𝗇 :</b>",
+            reply_markup=gen_key,
+        )
+    elif query.startswith("pyrogram") or query.startswith("telethon"):
+        try:
+            if query == "pyrogram":
+                await gen_session(cq.message, cq.from_user.id)
+            elif query == "pyrogram1":
+                await gen_session(cq.message, cq.from_user.id, old_pyro=True)
+            elif query == "telethon":
+                await gen_session(cq.message, cq.from_user.id, telethon=True)
+        except Exception as e:
+            await cq.edit_message_text(e, disable_web_page_preview=True)
