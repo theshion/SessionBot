df75aa445ee5a52264e81b3cc7ad347e713b1501
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-06 18:55:28 +0530

    Update stats.py

--- SessionBot/Plugins/stats.py
+++ SessionBot/Plugins/stats.py
@@ -1,1 +1,7 @@
+from pyrogram import Client as xemishra, filters
+from SessionBot import OWNER_ID
+from Database.users import get_users
 
+
+    users = len(await get_users())
+    await message.reply_text("")
