33eebdcb44b8b0eee711e1aec03ce0da5c89d6db
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-06 17:16:58 +0530

    Update __init__.py

--- SessionBot/__init__.py
+++ SessionBot/__init__.py
@@ -1,1 +1,9 @@
+import config
 
+
+API_ID = config.API_ID
+API_HASH = config.API_HASH
+BOT_TOKEN = config.BOT_TOKEN
+MONGODB_URI = config.MONGODB_URI
+LOGGER_ID = config.LOGGER_ID
+OWNER_ID = config.OWNER_ID
