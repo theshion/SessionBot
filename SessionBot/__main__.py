0067d68d0b4ab8730f9c2fad5478cfb2e2448f60
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-07 12:21:44 +0530

    Update __main__.py

--- SessionBot/__main__.py
+++ SessionBot/__main__.py
@@ -1,12 +1,3 @@
-import asyncio
-from pyrogram import Client, idle
 from SessionBot import xemishra
 
-async def install():
-  await xemishra.start()
-  x = await xemishra.get_me()
-  print("Your SessionBot {x.first_name} Has Been Started Successfully!")
-  await idle()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(install())
+xemishra.run()
