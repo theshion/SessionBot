e4d005ab82cb0e9e3201310dc75a4c373f45ce8f
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-07 22:45:19 +0530

    Update __init__.py

--- SessionBot/__init__.py
+++ SessionBot/__init__.py
@@ -3,17 +3,36 @@ import config
 import logging
 from pyrogram import Client
 from datetime import datetime
+import pyromod
+from pyrogram.enums import ParseMode
 
 # logging 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
 # creating pyrogram client 
-xemishra = Client(
-  "SessionBot",
-  api_id=config.API_ID,
-  api_hash=config.API_HASH,
-  bot_token=config.BOT_TOKEN,
-)
+class xemishra(Client):
+    def __init__(self):
+        super().__init__(
+            name="SessionBot",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+    async def stop(self):
+        await super().stop()
+
+
+xemishra = xemishra()
 
 # storing start time for ping command
 StartTime = time.time()
