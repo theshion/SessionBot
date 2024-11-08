ff1c5f3a40d93f1358eba50434a8f4c0f464ca80
{🇮🇳} シヴァ <xemishra@gmail.com>
2024-11-08 18:24:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ API_ID = int(os.getenv("API_ID", "28102624")) # get it from my.telegram.org and 
 API_HASH = os.getenv("API_HASH", "4e03913f9a576278ed4dbcdf7073e1b0") # get it from my.telegram.org and fill here.
 
 #Tokens
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7737621490:AAHnAjyvw1mxzsviGGCVuiCIPdx4gqR9nd0") # get it from @BotFather on telegram by creating a new bot and fill here that bot token.
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7939164806:AAFM_IcYWxPqVVbKGbdlEThnXLQ4qHS1AYA") # get it from @BotFather on telegram by creating a new bot and fill here that bot token.
 
 #Database
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://xemishra:3z8blpvVeMcKoZHs@truecallerxebot.46tc2.mongodb.net/?retryWrites=true&w=majority&appName=TrueCallerXeBot") # get it from mongodb.com and fill here.
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://xzcshion:Kalpit666@cluster0.okntwly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # get it from mongodb.com and fill here.
 
 #Owner
-LOGGER_ID = int(os.getenv("LOGGER_ID", "7644065212")) # get it by creating a logs group on telegram.
-OWNER_ID = int(os.getenv("OWNER_ID", "7644065212")) # flll here your bot owner telegram account id.
+LOGGER_ID = int(os.getenv("LOGGER_ID", "-1002391320301")) # get it by creating a logs group on telegram.
+OWNER_ID = int(os.getenv("OWNER_ID", "7050270303")) # flll here your bot owner telegram account id.
 
 #Extra 
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/xemishra") # fill here your bot support group link 
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/xemishra") # fill here your updates channel link here 
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/coding") # fill here your bot support group link 
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/coding") # fill here your updates channel link here
